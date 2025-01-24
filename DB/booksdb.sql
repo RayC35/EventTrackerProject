0884f2616069275f881e0834209e1dd659c2272e
RaymondCostales <raymond.w.costales@gmail.com>
2025-01-24 16:32:17 -0700

    Create schema model and database

---
+++ DB/booksdb.sql
@@ -0,0 +1,53 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema booksdb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `booksdb` ;
+
+-- -----------------------------------------------------
+-- Schema booksdb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `booksdb` ;
+USE `booksdb` ;
+
+-- -----------------------------------------------------
+-- Table `book`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `book` ;
+
+CREATE TABLE IF NOT EXISTS `book` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(200) NULL,
+  `author` VARCHAR(200) NULL,
+  `create_date` DATETIME NULL,
+  `last_update` DATETIME NULL,
+  `date_published` DATETIME NULL,
+  `synopsis` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+SET SQL_MODE = '';
+DROP USER IF EXISTS student@localhost;
+SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+CREATE USER 'student'@'localhost' IDENTIFIED BY 'student';
+
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'student'@'localhost';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `book`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `booksdb`;
+INSERT INTO `book` (`id`, `title`, `author`, `create_date`, `last_update`, `date_published`, `synopsis`) VALUES (1, 'The Winter of Our Discontent', 'John Steinbeck', '2024-03-21', '2000-04-17', '1878-09-18', 'stuff happens');
+
+COMMIT;
+
