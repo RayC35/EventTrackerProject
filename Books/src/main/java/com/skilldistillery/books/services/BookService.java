417dd35434e1966f3fca7ee2bd07fbbdc15d4151
RaymondCostales <raymond.w.costales@gmail.com>
2025-01-24 18:10:27 -0700

    Set up Boot REST project, full-stack smoke test

---
+++ Books/src/main/java/com/skilldistillery/books/services/BookService.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.books.services;
+
+import java.util.List;
+
+import com.skilldistillery.books.entities.Book;
+
+public interface BookService {
+	List<Book> findAll();
+	Book findById(int bookId);
+	Book create(Book book);
+	Book update(int bookId, Book book);
+	boolean deleteById(int bookId);
+	
+
+}
