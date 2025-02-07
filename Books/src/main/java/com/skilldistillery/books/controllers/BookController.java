d57b628930bf4713106a36886c7fa1d9bb563acf
RaymondCostales <raymond.w.costales@gmail.com>
2025-02-07 11:32:25 -0700

    Add crossorigin to controllers

--- Books/src/main/java/com/skilldistillery/books/controllers/BookController.java
+++ Books/src/main/java/com/skilldistillery/books/controllers/BookController.java
@@ -2,8 +2,8 @@ package com.skilldistillery.books.controllers;
 
 import java.util.List;
 
-import org.hibernate.boot.model.source.spi.HibernateTypeSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +18,7 @@ import com.skilldistillery.books.services.BookService;
 
 import jakarta.servlet.http.HttpServletResponse;
 
+@CrossOrigin({"*", "http://localhost/"})
 @RestController
 @RequestMapping("api")
 public class BookController {
